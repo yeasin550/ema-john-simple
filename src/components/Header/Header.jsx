6ca22621df1829c3a04d9e9d40b924ca7ea78a48
yeasin550 <yeasin51570@gmail.com>
2023-03-26 23:24:16 +0600

    header section added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+import logo from "../../images/Logo.svg"
+const Header = () => {
+    return (
+      <nav className="header">
+        <img src={logo} alt="" />
+        <div>
+            <a href="/Order">Order</a>
+            <a href="/Order Review">Order Review</a>
+            <a href="/Manage Inventory">Manage Inventory</a>
+            <a href="/Login">Login</a>
+        </div>
+      </nav>
+    );
+};
+
+export default Header;
