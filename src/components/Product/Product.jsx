5713972ed4aa90fbe0d09407831a734d9f6c39ee
yeasin550 <yeasin51570@gmail.com>
2023-03-27 01:10:16 +0600

    all data card showing on display

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const { name, img, quantity, shipping, price } = props.product;
+    const { name, img, quantity, shipping, price, seller, ratings } =
+      props.product;
     console.log(props.product);
     return (
       <div className="product">
-            <img src={img} alt="" />
-            <h5>{ name}</h5>
+        <img src={img} alt="" />
+        <div className='product-info'>
+          <h5 className="product-name">{name}</h5>
+          <p>Price : ${price}</p>
+          <p>Manufacturer : {seller}</p>
+          <p>
+            Rating : {ratings} <i class="fa-solid fa-star fa-spin"></i>
+          </p>
+        </div>
+        <button className='btn-cart'>
+          Add to Cart <i class="fa-solid fa-cart-plus fa-bounce"></i>
+        </button>
       </div>
     );
 };
